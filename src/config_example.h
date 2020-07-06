3f943d14fec8c06344d653ad2cff69c939cdff67
Gleb K <49366600+Glebonsky@users.noreply.github.com>
2020-07-06 19:11:31 +0300

    Added config example

---
+++ src/config_example.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "arduinoonpc.h"
+
+const String ssid = "your ssid";
+const String password = "your password";
+
+const String token = "your bot token";
+
+#endif // CONFIG_H
