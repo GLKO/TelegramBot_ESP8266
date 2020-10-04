0386a050992261222e490a9439d08adffdecf24a
Gleb K <49366600+Glebonsky@users.noreply.github.com>
2020-10-04 12:25:40 +0300

    Keyboard class changed into template, reply keyboard added

--- src/telegramobjects.cpp
+++ src/telegramobjects.cpp
@@ -1,12 +1,49 @@
 #include "telegramobjects.h"
 
 
-TelegramTextMessage::TelegramTextMessage(String chat_id, String text)
-    : _chat_id(chat_id),
-      _text(text)
+TelegramMessage::TelegramMessage(String text)
+    : _text(text)
 {}
 
-String TelegramTextMessage::json() const
+TelegramMessage::TelegramMessage(String text, String chatId)
+    : _text(text),
+      _chat_id(chatId)
+{}
+
+String TelegramMessage::json() const
 {
     return "chat_id=" + _chat_id + "&text=" + _text;
+}
+
+
+TelegramComplexMessage::TelegramComplexMessage(const TelegramMessage &textMessage, const TelegramObject &optionalParameter)
+    : _textMessage(textMessage),
+      _optionalParameter(optionalParameter)
+{}
+
+String TelegramComplexMessage::json() const
+{
+    return _textMessage.json() + "&" + _optionalParameter.json();
+}
+
+
+
+TelegramInlineButton::TelegramInlineButton(String text, String callbackData)
+    : _text(text),
+      _callbackData(callbackData)
+{}
+
+String TelegramInlineButton::json() const
+{
+    return "{\"text\":\"" + _text + "\",\"callback_data\":" + _callbackData + "}";
+}
+
+
+TelegramReplyButton::TelegramReplyButton(String text)
+    : _text(text)
+{}
+
+String TelegramReplyButton::json() const
+{
+    return "{\"text\":\"" + _text + "\",\"request_poll\":{\"type\":\"regular\"}}"; //fix optional data
 }
--- src/telegramobjects.h
+++ src/telegramobjects.h
@@ -1,7 +1,7 @@
 #ifndef TELEGRAMOBJECTS_H
 #define TELEGRAMOBJECTS_H
 
-#include "arduinoonpc.h"
+#include "mystring.h"
 
 class TelegramObject
 {
@@ -10,39 +10,128 @@ public:
 };
 
 
-class TelegramTextMessage : public TelegramObject
+class TelegramMessage : public TelegramObject
 {
 public:
-    TelegramTextMessage(String chat_id, String text);
+    TelegramMessage(String text); //CRUTCH
+    TelegramMessage(String text, String chatId);
     String json() const override;
 
 private:
-    const String _chat_id;
     const String _text;
+    const String _chat_id = "420638906"; //CRUTCH
+};
+
+class TelegramComplexMessage : public TelegramObject
+{
+public:
+    TelegramComplexMessage(const TelegramMessage &textMessage, const TelegramObject &optionalParameter);
+    String json() const override;
+
+private:
+    const TelegramMessage &_textMessage;
+    const TelegramObject &_optionalParameter;
 };
 
 
+
+class TelegramInlineButton : public TelegramObject
+{
+public:
+    TelegramInlineButton(String text, String callbackData);
+    String json() const override;
+
+private:
+    const String _text;
+    const String _callbackData;
+};
+
+//template used instead dynamic array
+template<int size>
 class TelegramInlineKeyboard : public TelegramObject
 {
 public:
-    TelegramInlineKeyboard();
-    String json() const override;
+    TelegramInlineKeyboard() {}
+    void addButton(TelegramInlineButton *button)
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            if ( _buttons[i] != nullptr ) continue;
+
+            _buttons[i] = button;
+            break;
+        }
+    }
+
+    String json() const override
+    {
+        String result = "reply_markup={\"inline_keyboard\":[";
+
+        for (int i = 0; i < size; ++i)
+        {
+            result = result + "[" + _buttons[i]->json() + "]";
+
+            if ( i != size-1 )
+            {
+                result = result + ",";
+            }
+        }
+        result = result + "]}";
+        return result;
+    }
 
 private:
-
+    TelegramInlineButton *_buttons[size] = {nullptr};
 };
 
 
-class TelegramInlineKeyboardMessage : public TelegramObject
+
+class TelegramReplyButton : public TelegramObject
 {
 public:
-    TelegramInlineKeyboardMessage(const TelegramTextMessage &textMessage, String reply_markup);
+    TelegramReplyButton(String text);
     String json() const override;
 
 private:
-    const TelegramTextMessage _textMessage;
-    const TelegramInlineKeyboard _inlineKeyboard;
+    const String _text;
 };
 
+//template used instead dynamic array
+template<int size>
+class TelegramReplyKeyboard : public TelegramObject
+{
+public:
+    TelegramReplyKeyboard() {}
+    void addButton(TelegramReplyButton *button)
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            if ( _buttons[i] != nullptr ) continue;
+
+            _buttons[i] = button;
+            break;
+        }
+    }
+
+    String json() const override
+    {
+        String result = "reply_markup={\"keyboard\":[";
+
+        for (int i = 0; i < size; ++i)
+        {
+            result = result + "[" + _buttons[i]->json() + "]";
+
+            if ( i != size-1 )
+            {
+                result = result + ",";
+            }
+        }
+        result = result + "]}";
+        return result;
+    }
+
+private:
+    TelegramReplyButton *_buttons[size] = {nullptr};
+};
 
 #endif // TELEGRAMOBJECTS_H
